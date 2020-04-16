1c77360693d9d3cc3fcd6470c9132d76a925b47c
Rahul Rai <rahulrai@ucreate.co.in>
2020-04-16 11:19:05 +0530

    Working with Dynamic

--- app.js
+++ app.js
@@ -4,17 +4,21 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
-const adminRoutes = require('./routes/admin');
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/admin', adminRoutes);
+app.use('/admin', adminData.router);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
     res.status(404)
-    .sendFile(path.join(__dirname, 'views', '404.html'));
+    .render('404', {pageTitle: 'page not found', path:''})
 });
 
 app.listen(3000);
