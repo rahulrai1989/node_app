516264a38428e078a5d76c3ac354c38f0e71a849
Rahul Rai <rahulrai@ucreate.co.in>
2020-06-09 18:38:20 +0530

    config changes

--- config/config.js
+++ config/config.js
@@ -12,16 +12,14 @@ const config = {
       "password": process.env.DB_PASS,
       "database": process.env.DB_NAME,
       "host": process.env.DB_HOST,
-      "dialect": "postgres",
-      "operatorsAliases": false
+      "dialect": "postgres"
     },
     "production": {
       "username": process.env.DB_USER,
       "password": process.env.DB_PASS,
       "database": process.env.DB_NAME,
       "host": process.env.DB_HOST,
-      "dialect": "postgres",
-      "operatorsAliases": false
+      "dialect": "postgres"
     }
 };
 
