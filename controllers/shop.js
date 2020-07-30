ae1fed5b0c6d7c2a26f282973776403ef7d596bc
Rahul Rai <rahulrai@ucreate.co.in>
2020-07-30 11:42:53 +0530

    Create functionality to add orders and get orders detail using sequlize relations and remove unused route and

--- controllers/shop.js
+++ controllers/shop.js
@@ -100,16 +100,42 @@ exports.postCartDeleteProduct = (req, res, next) => {
   .catch(err => console.log(err));
 };
 
-exports.getOrders = (req, res, next) => {
-  res.render('shop/orders', {
-    path: '/orders',
-    pageTitle: 'Your Orders'
-  });
+exports.postOrders = (req, res, next) => {
+  let fetchedCart;
+  req.user.getCart()
+  .then(cart => {
+    fetchedCart = cart;
+    return cart.getProducts();
+  })
+  .then(products => {
+    return req.user
+    .createOrder()
+    .then(order => {
+      return order.addProducts(products.map(prod => {
+        prod.orderitem = { quantity: prod.cartitem.quantity }  
+          return prod; 
+        })
+      );
+    })
+    .then(results => {
+      return fetchedCart.setProducts(null);
+    })
+    .then(results => {
+      res.redirect('/orders');
+    })
+    .catch(err => console.log(err));
+  })
+  .catch(err => console.log(err));
 };
 
-exports.getCheckout = (req, res, next) => {
-  res.render('shop/checkout', {
-    path: '/checkout',
-    pageTitle: 'Checkout'
-  });
+exports.getOrders = (req, res, next) => {
+  req.user.getOrders({include: ['products']})
+  .then(orders => {
+    res.render('shop/orders', {
+      path: '/orders',
+      pageTitle: 'Your Orders',
+      orders: orders
+    });
+  })
+  .catch(err => console.log(err));
 };
