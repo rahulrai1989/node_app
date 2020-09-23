85d89401866db67d4c7db66a4a0382d9a4b66195
Rahul Rai <rahulrai@ucreate.co.in>
2020-09-23 11:00:50 +0530

    package json update and Initializing the Session Middleware

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const dotenv = require('dotenv').config();
 const { user } = require('./models');
+const session = require('express-session');
 
 
 const errorController = require('./controllers/error');
@@ -18,6 +19,7 @@ const authRoutes = require('./routes/auth');
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({ secret: 'my secret', resave: false, saveUninitialized: false }));
 
 app.use((req, res, next) => {
     user.findByPk(1)
